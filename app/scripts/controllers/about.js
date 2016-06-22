11af53a5c069da232e16cd64ab6f1b1b27923c21
eXt-Ra <vanardois.romain@gmail.com>
2016-06-22 14:12:24 +0200

    rebuild yo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transaldisAdminApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the transaldisAdminApp
+ */
+angular.module('transaldisAdminApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
