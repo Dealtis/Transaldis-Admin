d57ecd0f02e771c27a4d975eaf36058a822ec085
eXt-Ra <vanardois.romain@gmail.com>
2016-06-22 11:21:03 +0200

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name transaldisAdminApp
+ * @description
+ * # transaldisAdminApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('transaldisAdminApp', [
+    'ngAnimate',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transaldisAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the transaldisAdminApp
+ */
+angular.module('transaldisAdminApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
